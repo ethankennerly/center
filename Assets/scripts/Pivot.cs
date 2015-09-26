b457c054b55e8130088d0ec0e2d25ada24dafdd5
Ethan Kennerly <ethan@lumoslabs.com>
2015-09-26 11:05:14 -0700

    Plate tilts horizontally when mouse not in center

---
+++ Assets/scripts/Pivot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pivot : MonoBehaviour {
+	public float torqueMultiplier;
+	public float torqueVertical;
+	public float torqueHorizontal;
+	public Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.FreezePosition;
+	}
+
+	void TorqueAxis() {
+		torqueHorizontal =  Input.GetAxis ("Mouse X");
+		torqueVertical =  Input.GetAxis ("Mouse Y");
+	}
+
+	void TorquePosition() {
+		torqueHorizontal =  (Input.mousePosition.x - 0.5f * Screen.width) / Screen.width;
+		torqueVertical =  (Input.mousePosition.y - 0.5f * Screen.height) / Screen.height;
+	}
+
+	void FixedUpdate () {
+		// TorqueAxis ();
+		TorquePosition ();
+		rb.AddTorque (0, 0, Time.deltaTime * -torqueHorizontal * torqueMultiplier);
+		// rb.AddTorque (transform.right * torqueVertical * torqueMultiplier);
+	}
+}
